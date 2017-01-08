ebf9bbc9483eb2d1f2ed87ae10aef057bc93d63c
Chris.V <cvasqu09@gmail.com>
2017-01-07 23:39:40 -0700

    Added tests to remove value and renamed tests.

--- LinkedLists/test/LinkedListOperationsTest.java
+++ LinkedLists/test/LinkedListOperationsTest.java
@@ -5,13 +5,13 @@ public class LinkedListOperationsTest {
 	LinkedList l = new LinkedList();
 
 	@Test
-	public void testAddToHeadSingleNode() {
+	public void testAddToHead_singleNode() {
 		l.addToHead(2);
 		Assert.assertEquals(2, l.getHead().getVal(), 0);
 	}
 	
 	@Test
-	public void testAddToHeadMultipleNodes(){
+	public void testAddToHead_multipleNodes(){
 		l.addToHead(3);
 		l.addToHead(4);
 		l.addToHead(5);
@@ -19,13 +19,13 @@ public class LinkedListOperationsTest {
 	}
 	
 	@Test
-	public void testAddToTailSingleNode(){
+	public void testAddToTail_singleNode(){
 		l.addToTail(2);
 		Assert.assertEquals(2, l.getTail().getVal(), 0);
 	}
 
 	@Test
-	public void testAddToTailMultipleNodes(){
+	public void testAddToTail_multipleNodes(){
 		l.addToTail(2);
 		l.addToTail(3);
 		l.addToTail(4);
@@ -33,53 +33,67 @@ public class LinkedListOperationsTest {
 	}
 	
 	@Test
-	public void testGetValueNoElements(){
-		Assert.assertEquals(-1, l.getValue(0));
+	public void testGetValue_noElements(){
+		Assert.assertEquals(-1, l.getValueAt(0));
 	}
 	
 	@Test
-	public void testGetValueMultipleElements(){
+	public void testGetValue_multipleElements(){
 		l.addToTail(3);
 		l.addToTail(4);
 		l.addToTail(5);
-		Assert.assertEquals(4, l.getValue(1));
+		Assert.assertEquals(4, l.getValueAt(1));
 	}
 	
 	@Test
-	public void testGetValueOutOfRange(){
+	public void testGetValue_outOfRange(){
 		l.addToTail(3);
-		Assert.assertEquals(-1, l.getValue(100));
+		Assert.assertEquals(-1, l.getValueAt(100));
 	}
 	
 	@Test
-	public void testQuickSortValid(){
+	public void testQuickSort_valid(){
 		l.addToTail(5);
 		l.addToTail(4);
 		l.addToTail(3);
 		l.quicksort(0, 2);
 		// Assert everything is in order
 		Assert.assertEquals(3, l.getHead().getVal(), 0);
-		Assert.assertEquals(4, l.getValue(1), 0);
-		Assert.assertEquals(5, l.getValue(2), 0);
+		Assert.assertEquals(4, l.getValueAt(1), 0);
+		Assert.assertEquals(5, l.getValueAt(2), 0);
 	}
 	
 	@Test
-	public void testQuickSortIndicesOutOfRange(){
+	public void testQuickSort_indicesOutOfRange(){
 		l.addToHead(2);
 		l.addToHead(1);
 		l.addToHead(3);
 		l.quicksort(-2, 3);
 		Assert.assertEquals(3, l.getHead().getVal(), 0);
-		Assert.assertEquals(1, l.getValue(1), 0);
-		Assert.assertEquals(2, l.getValue(2), 0);
+		Assert.assertEquals(1, l.getValueAt(1), 0);
+		Assert.assertEquals(2, l.getValueAt(2), 0);
 	}
 	
 	@Test
-	public void testSwapValid(){
+	public void testSwap_valid(){
 		l.addToTail(2);
 		l.addToTail(3);
 		l.addToTail(4);
 		l.swap(l.getNode(0), l.getNode(2));
+		Assert.assertEquals(4, l.getHead().getVal(), 0);
+		Assert.assertEquals(2, l.getTail().getVal());
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testRemoveVal_nullList(){
+		l.removeVal(4);
+	}
+	
+	@Test
+	public void testRemoveVal_singleElement(){
+		l.addToHead(2);
+		l.removeVal(2);
+		Assert.assertEquals(0, l.getNumElements(), 0);
 	}
 
 	
